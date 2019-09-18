13f8bf5f43d5a8202751a8fe22d99034b30c286a
magliana22 <magliana22@up.edu>
2019-09-17 17:18:40 -0700

    Pair B Checkpoint 1

--- app/src/main/java/cs301/birthdaycake/CakeView.java
+++ app/src/main/java/cs301/birthdaycake/CakeView.java
@@ -27,7 +27,7 @@ public class CakeView extends SurfaceView {
     public static final float cakeWidth = 1200.0f;
     public static final float layerHeight = 200.0f;
     public static final float frostHeight = 50.0f;
-    public static final float candleHeight = 300.0f;
+    public static final float candleHeight = 200.0f;
     public static final float candleWidth = 40.0f;
     public static final float wickHeight = 30.0f;
     public static final float wickWidth = 6.0f;
